2fe03f9cf63135b8e1f361318c05b2bb5cf05896
ampatte <ampatterson1@hotmail.com>
2022-12-06 06:39:50 -0500

    fetch two api calls

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # What's the Weather?
+
+
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+fetch(requestURL)
+.then(function(response){
+    return response.json();
+})
---
+++ script.js
@@ -0,0 +1,49 @@
+var apiKey= `68ef797c869fb49c98d4912d6f16f681`;
+var city='';
+
+//form inputs
+function citySearch() {
+    var city= document.getElementById("city").value;
+    document.getElementById("city").innerHTML=city
+    console.log(city)
+    
+//querycity search
+
+   var firstCall= `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+   fetch(firstCall) 
+  .then((response) =>response.text())
+  .then(data=> setCoordinates(data))
+  .then(data=> {console.log(data)})
+
+}
+document.getElementById("searchBtn").addEventListener("click",  citySearch);
+
+//display current and future weather conditions
+//&
+//city search history is saved in local storage
+function setCoordinates(mapData) {
+    var coordinates= JSON.parse(mapData);
+    var lat= coordinates[0].lat;
+    var lon= coordinates[0].lon;
+    var secondCall=`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+//lat & lon data= coordinates
+    fetch(secondCall)
+    .then((response)=> response.text())
+    .then(data=> setWeatherData(data));
+    console.log(mapData)
+
+    document.getElementById("callOne").innerHTML= mapData;
+}
+//setCoordinates(mapData)
+//weather conditions include:
+//cityName,date,weatherIcon,temp,humidity,windSpeed
+function setWeatherData(forecastData){
+
+var weatherData= JSON.parse(forecastData);
+document.getElementById("callTwo").innerHTML = forecastData;
+console.log(callTwo)
+//future 5day forecast includes:
+//date,weatherIcon,temp,windSpeed,humidity
+console.log("list")
+//button/onclick city in searchHistory for current and future forecasts
+}
